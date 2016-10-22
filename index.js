c0f7503b51898e2fc9f5d602f54a7df5c6524f30
Ire Aderinokun <ire@ireaderinokun.com>
2016-10-22 19:59:04 +0100

    sendTweet.js

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 /* ******************
 
-	Fixes for Heroku
-	See http://stackoverflow.com/a/31094668
+ Fixes for Heroku
+ See http://stackoverflow.com/a/31094668
 
-******************* */
+ ******************* */
 
 const express = require('express');
 const app = express();
 app.set('port', (process.env.PORT || 5000));
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
     var result = 'App is running'
     response.send(result);
-}).listen(app.get('port'), function() {
+}).listen(app.get('port'), function () {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
 
 
 /* ******************
 
-	The Setup
+ The Setup
 
-******************* */
+ ******************* */
 const Twit = require('twit');
 const T = new Twit({
 	consumer_key: process.env.consumer_key,
@@ -32,17 +32,17 @@ const T = new Twit({
 
 /* ******************
 
-	Key Variables
+ Key Variables
 
-******************* */
+ ******************* */
 const me = {
-	id: 2714960622,
-	screen_name: 'ireaderinokun'
+    id: 2714960622,
+    screen_name: 'ireaderinokun'
 };
 
 const botsofcode = {
-	id: 743145993844179000,
-	screen_name: 'botsofcode'
+    id: 743145993844179000,
+    screen_name: 'botsofcode'
 };
 
 const emojis = ['👊', '👊', '🙌', '👍', '💁', '👌', '🙅', '👯'];
@@ -50,63 +50,60 @@ const emojis = ['👊', '👊', '🙌', '👍', '💁', '👌', '🙅', '👯'];
 
 /* ******************
 
-	General Functions
+ General Functions
 
-******************* */
+ ******************* */
 function shouldSendReply() {
-	const randomNumber = Math.random();
-	if ( randomNumber > 0.3 ) return true;
-	return false;
+    const randomNumber = Math.random();
+    if (randomNumber > 0.3) return true;
+    return false;
 }
 
 function getEmoji() {
-	return emojis[Math.floor(Math.random() * emojis.length)];
+    return emojis[Math.floor(Math.random() * emojis.length)];
 }
 
 
 /* ******************
 
-	Tweet Functions
+ Tweet Functions
 
-******************* */
+ ******************* */
 const tweet = (tweet) => {
-	T.post('statuses/update', {
-		status: tweet
-	});
+    T.post('statuses/update', {
+        status: tweet
+    });
 }
 
 const retweet = (tweet) => {
-	T.post('statuses/retweet/:id', { id: tweet.id_str });
+    T.post('statuses/retweet/:id', {id: tweet.id_str});
 };
 
 const reply = (tweet, reply) => {
-	T.post('statuses/update', {
-		status: `@${tweet.user.screen_name} ${reply}`,
-		in_reply_to_status_id: tweet.id_str
-	});
+    T.post('statuses/update', {
+        status: `@${tweet.user.screen_name} ${reply}`,
+        in_reply_to_status_id: tweet.id_str
+    });
 }
 
 const like = (tweet) => {
-	T.post('favorites/create', { id: tweet.id_str });
+    T.post('favorites/create', {id: tweet.id_str});
 }
 
 const addToList = (list, user) => {
-	T.post('lists/members/create', {
-		slug: list,
-		owner_screen_name: botsofcode.screen_name,
-		screen_name: user
-	});
+    T.post('lists/members/create', {
+        slug: list,
+        owner_screen_name: botsofcode.screen_name,
+        screen_name: user
+    });
 }
-
-
-
 
 
 /* ******************
 
-	Stream
+ Stream
 
-******************* */
+ ******************* */
 
 const stream = T.stream('statuses/filter', { track: ['bitsofco.de', 'bitsofcode'] });
 
@@ -128,8 +125,15 @@ stream.on('tweet', (tweet) => {
 			reply(tweet, `Thanks for sharing! ${ getEmoji() }`);
 		}
 		return;
-	} 
+	}
 
 	reply(tweet, `Thanks for sharing! ${ getEmoji() } (cc @${me.screen_name})`);
 
 });
+
+
+
+
+
+
+
---
+++ sendTweet.js
@@ -0,0 +1,82 @@
+const Twit = require('twit');
+const T = new Twit({
+	consumer_key: process.env.consumer_key,
+	consumer_secret: process.env.consumer_secret,
+	access_token: process.env.access_token,
+	access_token_secret: process.env.access_token_secret
+});
+const https = require('https');
+const tweet = (tweet) => {
+    T.post('statuses/update', {
+        status: tweet
+    });
+}
+const url = 'https://ireade.github.io/bitsofcode-backups/bitsofcode.ghost.2016-09-30.json';
+
+function fetch(url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, function (res) {
+            let body = '';
+            res.on('data', function (data) {
+                data = data.toString();
+                body += data;
+            });
+
+            res.on('end', function () {
+                body = JSON.parse(body);
+                resolve(body);
+            });
+
+        }).on('error', function (err) {
+            reject(err);
+        });
+
+    })
+}
+
+
+function createTweet(post, tags) {
+    let hashtags = '';
+    tags.map(tag => hashtags += `#${tag} `)
+    const tweetText = `'${post.title}' https://bitsofco.de/${post.slug} ${hashtags}`;
+    console.log(tweetText);
+    tweet(tweetText);
+}
+
+fetch(url).then((response) => {
+
+    const posts = response.db[0].data.posts;
+    const posts_tags = response.db[0].data.posts_tags;
+    const tags = response.db[0].data.tags;
+
+    function getRandomPost() {
+        for (let i = 0; i < posts.length; i++) {
+            let post = posts[Math.floor(Math.random()*posts.length)];
+            let isPost = post.page === 0;
+            let isPublished = post.status === 'published';
+            let isPublic = post.visibility === 'public';
+            if ( isPost && isPublished && isPublic ) {
+                return post;
+                break;
+            }
+        }
+    }
+
+    function getPostTags(postID) {
+        const postTagsIds = [];
+        posts_tags.find((posts_tag) => {
+            if ( posts_tag.post_id === postID ) postTagsIds.push(posts_tag.tag_id);
+        });
+        const postTags = [];
+        postTagsIds.map((postTagsID) => {
+            tags.find((tag) => {
+                if ( tag.id === postTagsID ) return postTags.push(tag.name);
+            })
+        })
+        return postTags;
+    }
+
+    const randomPost = getRandomPost();
+    const randomPostTags = getPostTags(randomPost.id);
+    createTweet(randomPost, randomPostTags)
+})
