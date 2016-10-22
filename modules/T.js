54a492853e87ceb16d64f20179e35c93e9bc973d
Ire Aderinokun <ire@ireaderinokun.com>
2016-10-22 20:59:28 +0100

    bitsofcode_backup_url in environement variable

--- modules/T.js
+++ modules/T.js
@@ -1,8 +1,7 @@
 const Twit = require('twit');
-const T = new Twit({
+module.exports = new Twit({
     consumer_key: process.env.consumer_key,
     consumer_secret: process.env.consumer_secret,
     access_token: process.env.access_token,
     access_token_secret: process.env.access_token_secret
 });
-module.exports = T;
