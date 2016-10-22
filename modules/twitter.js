6efa1c5681bdff40a973c605c8df18150e8a8dbb
Ire Aderinokun <ire@ireaderinokun.com>
2016-10-22 20:20:04 +0100

    modules

---
+++ modules/twitter.js
@@ -0,0 +1,32 @@
+const T = require('./modules/T');
+
+module.exports = {
+    tweet: (tweet) => {
+        T.post('statuses/update', {
+            status: tweet
+        });
+    },
+
+    retweet: (tweet) => {
+        T.post('statuses/retweet/:id', {id: tweet.id_str});
+    },
+
+    reply: (tweet, reply) => {
+        T.post('statuses/update', {
+            status: `@${tweet.user.screen_name} ${reply}`,
+            in_reply_to_status_id: tweet.id_str
+        });
+    },
+
+    like: (tweet) => {
+        T.post('favorites/create', {id: tweet.id_str});
+    },
+
+    addToList: (list, user) => {
+        T.post('lists/members/create', {
+            slug: list,
+            owner_screen_name: botsofcode.screen_name,
+            screen_name: user
+        });
+    }
+}
